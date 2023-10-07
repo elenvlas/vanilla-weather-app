c8515a09306ae1cf90d0cb67f4e4bdb4bc029da2
elenvlas <argex@ukr.net>
2023-10-07 16:38:54 +0300

    Added relevant icon from the API

--- crs/app.js
+++ crs/app.js
@@ -28,16 +28,24 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "7c1ebf49ca8b80e830e1b89eb9a39754";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kharkov&appid=${apiKey}&units=metric`;
+let city = "Kharkov";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
